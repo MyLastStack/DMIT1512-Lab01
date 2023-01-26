cbeb30da1f09afd19bd183ee43f8dc4e44255d5d
Johnne Geograpo <59752069+MyLastStack@users.noreply.github.com>
2023-01-26 09:21:54 -0700

    Need flip

--- Assets/MakePlanet.cs
+++ Assets/MakePlanet.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class MakePlanet : MonoBehaviour
 {
     [SerializeField] GameObject planet;
+    [SerializeField] GameObject background;
     GameObject p;
+    GameObject bg;
     // Start is called before the first frame update
     void Start()
     {
 
         // Make the planet when the camera starts in the scene.
         Camera cam = Camera.main;
-
+        bg = Instantiate(background, new Vector3(0f, 0f, 1f), Quaternion.identity);
 
         p = Instantiate(planet, new Vector2(3f, 2f), Quaternion.identity);
         p.transform.localScale = Vector2.one * 3f;
@@ -21,9 +23,6 @@ public class MakePlanet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.anyKeyDown)
-        {
-            Destroy(p);
-        }
+        
     }
 }
--- Assets/MerlinScripts.cs
+++ Assets/MerlinScripts.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine;
+using UnityEditor.Tilemaps;
 
 public class MerlinScripts : MonoBehaviour
 {
+    Vector2 ScreenPos;
+    Vector2 Direction = new(1, 0);
+    Vector2 ScreenSize;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-        transform.transform.localScale = new Vector3(5f, 5f, 5f);
+        ScreenSize = new(1920, 1080);
     }
 
     void FixedUpdate()
@@ -20,6 +24,25 @@ public class MerlinScripts : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Keyboard.current
+        if (Keyboard.current.aKey.isPressed)
+        {
+            transform.Translate(Vector2.left * 5f * Time.deltaTime);
+            //if (transform.rotation.y != 0)
+            //{
+            //    transform.
+            //}
+            
+        }
+        if (Keyboard.current.dKey.isPressed)
+        {
+            transform.Translate(Vector2.right * 5f * Time.deltaTime);
+            //if (transform.rotation.y != 0)
+            //{
+            //    transform.Rotate(0f, 0f, 0f);
+            //}
+        }
+        ScreenPos.x = transform.position.x;
+
+        //if (ScreenPos.x + )
     }
 }
