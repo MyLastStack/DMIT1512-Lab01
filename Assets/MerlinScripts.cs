3976a4e057e35473631da10094f4c41c4e3889dd
Johnne Geograpo <59752069+MyLastStack@users.noreply.github.com>
2023-01-26 09:44:42 -0700

    Flip added
    add bullets and enemies

--- Assets/MerlinScripts.cs
+++ Assets/MerlinScripts.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine;
 using UnityEditor.Tilemaps;
+using UnityEngine.Rendering;
 
 public class MerlinScripts : MonoBehaviour
 {
+    [SerializeField] GameObject Sploosh;
+
     Vector2 ScreenPos;
     Vector2 Direction = new(1, 0);
     Vector2 ScreenSize;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -24,22 +26,27 @@ public class MerlinScripts : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
+        if (Keyboard.current.spaceKey.wasReleasedThisFrame)
+        {
+
+        }
         if (Keyboard.current.aKey.isPressed)
         {
             transform.Translate(Vector2.left * 5f * Time.deltaTime);
-            //if (transform.rotation.y != 0)
-            //{
-            //    transform.
-            //}
-            
+            if (!sr.flipX)
+            {
+                sr.flipX = true;
+            }
         }
         if (Keyboard.current.dKey.isPressed)
         {
             transform.Translate(Vector2.right * 5f * Time.deltaTime);
-            //if (transform.rotation.y != 0)
-            //{
-            //    transform.Rotate(0f, 0f, 0f);
-            //}
+            if (sr.flipX)
+            {
+                sr.flipX = false;
+            }
         }
         ScreenPos.x = transform.position.x;
 
